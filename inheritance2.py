c7737b1ef3c28cce714ed680356265e6f227d7b4
Ahmad Furqan <157381667+ahmadfurqan7@users.noreply.github.com>
2024-07-19 14:02:18 +0700

    Add files via upload

---
+++ inheritance2.py
@@ -0,0 +1,38 @@
+# Parent class
+class Animal:
+  def __init__(self, name):
+    self.name = name
+
+  # Generic sound method for any animal
+  def sound(self):
+    print("Making a sound")
+
+# Child class Dog
+class Dog(Animal):
+  def __init__(self, name, breed, age):
+    super().__init__(name)
+    self.breed = breed
+    self.age = age
+  
+  # Overridden sound method for Dog
+  def sound(self):
+    print("Woof!")
+
+# Child class Cat
+class Cat(Animal):
+  def __init__(self, name, breed, age):
+    super().__init__(name)
+    self.breed = breed
+    self.age = age
+
+  # Overridden sound method for Cat
+  def sound(self):
+    print("Meow!")
+
+# Creating instances
+my_dog = Dog("Jax", "Bulldog", 5)
+my_cat = Cat("Lily", "Ragdoll", 2)
+
+# Using overridden methods
+my_dog.sound()
+my_cat.sound()
